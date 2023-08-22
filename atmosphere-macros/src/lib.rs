a6032dbead7aaeca7afbdd4b10ee793e38b07072
Mara Schulke <mara@schulke.xyz>
2023-08-22 18:18:50 +0200

    Update macro api

--- atmosphere-macros/src/lib.rs
+++ atmosphere-macros/src/lib.rs
@@ -1,3 +1,5 @@
+#![allow(unused)]
+
 use std::collections::HashMap;
 use std::sync::Mutex;
 
@@ -18,45 +20,103 @@ mod table;
 use database::{Database, Schema};
 use table::Table;
 
-#[proc_macro_derive(Table, attributes(primary_key, foreign_key))]
-pub fn table(input: TokenStream) -> TokenStream {
+#[proc_macro_derive(Schema, attributes(primary_key, foreign_key))]
+pub fn schema(input: TokenStream) -> TokenStream {
+    quote! {}.into()
+}
+
+// ----------------------------------------------------------------------------
+
+// Markers
+
+#[proc_macro_attribute]
+pub fn table(attr: TokenStream, input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
 
-    let Data::Struct(DataStruct {
-        fields: Fields::Named(FieldsNamed { named: columns, .. }),
-        ..
-    }) = &input.data
-    else {
-        panic!("Only named structs can derive the table trait");
+    //let params = parse_macro_input!(attr as syn::AttributeArgs);
+
+    let struct_name = &input.ident;
+
+    dbg!(struct_name.to_string());
+
+    let expanded = quote! {
+        #input
     };
 
-    let table = Table::parse(&input, &columns);
-
-    let mut db = (*Database).lock().unwrap();
-
-    let tid = (Schema::Public, table.ident.to_string());
-
-    assert!(
-        db.contains_key(&tid) == false,
-        "Unable to define the table {}: already exists",
-        table.ident.to_string(),
-    );
-
-    db.insert(tid, table.clone());
-
-    drop(db);
-
-    let table_impl = table.quote_table_impl();
-    let read_impl = table.quote_read_impl();
-    let write_impl = table.quote_write_impl();
-
-    quote! {
-        #table_impl
-        #read_impl
-        #write_impl
-    }
-    .into()
+    expanded.into()
 }
+
+#[proc_macro_attribute]
+pub fn relation(attr: TokenStream, input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+
+    //let params = parse_macro_input!(attr as syn::AttributeArgs);
+
+    let struct_name = &input.ident;
+
+    dbg!("rel", struct_name.to_string());
+
+    let expanded = quote! {
+        #input
+    };
+
+    expanded.into()
+}
+
+// ----------------------------------------------------------------------------
+
+// Query
+
+#[proc_macro_attribute]
+pub fn query(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    let item = parse_macro_input!(item as syn::Item);
+
+    dbg!("query", item.clone().into_token_stream().to_string());
+
+    let expanded = quote! { fn hi(&self) {} };
+
+    expanded.into()
+}
+
+//#[proc_macro_derive(Table, attributes(primary_key, foreign_key))]
+//pub fn table(input: TokenStream) -> TokenStream {
+//let input = parse_macro_input!(input as DeriveInput);
+
+//let Data::Struct(DataStruct {
+//fields: Fields::Named(FieldsNamed { named: columns, .. }),
+//..
+//}) = &input.data
+//else {
+//panic!("Only named structs can derive the table trait");
+//};
+
+//let table = Table::parse(&input, &columns);
+
+//let mut db = (*Database).lock().unwrap();
+
+//let tid = (Schema::Public, table.ident.to_string());
+
+//assert!(
+//db.contains_key(&tid) == false,
+//"Unable to define the table {}: already exists",
+//table.ident.to_string(),
+//);
+
+//db.insert(tid, table.clone());
+
+//drop(db);
+
+//let table_impl = table.quote_table_impl();
+//let read_impl = table.quote_read_impl();
+//let write_impl = table.quote_write_impl();
+
+//quote! {
+//#table_impl
+//#read_impl
+//#write_impl
+//}
+//.into()
+//}
 
 // Query Macros
 //#[proc_macro_attribute]
--- examples/forest/main.rs
+++ examples/forest/main.rs
@@ -1,61 +1,40 @@
 use atmosphere::prelude::*;
 use sqlx::{FromRow, PgPool};
 
-//#[table(name = "forest", schema = "public")]
-#[derive(Debug, PartialEq, FromRow, Table)]
+#[derive(Schema)]
+#[table(name = "forest", schema = "public")]
 struct Forest {
-    #[id]
+    #[primary_key]
     id: i32,
     name: String,
-    location: String,
 }
 
 impl Forest {
-    pub fn new(id: i32, name: impl AsRef<str>, location: impl AsRef<str>) -> Self {
+    pub fn new(id: i32, name: &str) -> Self {
         Self {
             id,
-            name: name.as_ref().to_owned(),
-            location: location.as_ref().to_owned(),
+            name: name.to_owned(),
         }
     }
 }
 
 impl Forest {
     /// Select a forest by its name
-    #[query]
-    pub async fn by_name(name: &str) -> Result<Self> {
-        sql! {
-            SELECT * FROM forest
-            WHERE name = $name
-            ORDER BY name
-        }
-    }
-
-    // Select all odd trees of this forest
-    //#[query]
-    //pub async fn trees(&self) -> Result<Tree> {
-    //sql! {
-    //SELECT * FROM tree
-    //WHERE forest_id = $self.id
-    //ORDER BY id
-    //}
-    //}
-
-    /// Burn down the forest
-    #[query]
-    pub async fn burn(&self) -> Result<()> {
-        sql! {
-            DELETE FROM tree WHERE forest_id = $self.id
-        }
-    }
+    #[query(
+        SELECT * FROM {Forest}
+        WHERE name = {name}
+        ORDER BY name
+    )]
+    pub async fn by_name(name: &str) -> Result<Self>;
 }
 
-//#[table(name = "tree", schema = "public")]
-#[derive(Debug, FromRow, Table)]
+#[derive(Schema)]
+#[table(name = "tree", schema = "public")]
+#[relation(grouped_by = Forest)]
 struct Tree {
-    #[id]
+    #[primary_key]
     id: i32,
-    #[reference(Forest)]
+    #[foreign_key(Forest)]
     forest_id: i32,
 }
 
@@ -64,30 +43,6 @@ async fn main() -> Result<()> {
     let pool = PgPool::connect(&std::env::var("DATABASE_URL").unwrap())
         .await
         .unwrap();
-
-    let grunewald = match Forest::find(&0, &pool).await {
-        Ok(forest) => forest,
-        Err(_) => {
-            let grunewald = Forest::new(0, "Grunewald", "Berlin");
-            grunewald.save(&pool).await?;
-            grunewald
-        }
-    };
-
-    let redwood = match Forest::find(&1, &pool).await {
-        Ok(forest) => forest,
-        Err(_) => {
-            let grunewald = Forest::new(0, "Redwood", "USA");
-            grunewald.save(&pool).await?;
-            grunewald
-        }
-    };
-
-    let forests = Forest::all(&pool).await?;
-    assert_eq!(&forests, &[grunewald, redwood]);
-
-    //assert_eq!(Forest::by_name("Grunewald", &pool).await?, grunewald);
-    //assert_eq!(Forest::by_name("Redwood", &pool).await?, redwood);
 
     Ok(())
 }
