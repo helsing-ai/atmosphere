9c2c4034d25a17aa545f2f9a439500ab0fd9a48d
Nicolas Vizzari-Trinquier <nicolas.vizzari@helsing.ai>
2025-04-05 14:06:34 +0200

    Add clippy lints

--- atmosphere-core/src/schema/mod.rs
+++ atmosphere-core/src/schema/mod.rs
@@ -170,14 +170,12 @@ pub mod column {
         }
 
         /// # Safety
-        //
-        /// We do treat this foreign key as a column of another table. This is not
-        /// smart to do - but can become necessary when doing complex joins. This
-        /// is memory safe as `Self<A>` and `Self<B>` have the exact same memory layout,
-        /// we do not store any data (A or B) but only a `PhantomData` instance which
-        /// is here transmuted.
+        /// We do treat this foreign key as a column of another table. This is not smart to do - but
+        /// can become necessary when doing complex joins. This is memory safe as `Self<A>` and
+        /// `Self<B>` have the exact same memory layout, we do not store any data (A or B) but only
+        /// a `PhantomData` instance which is here transmuted.
         pub const unsafe fn transmute<I: Table>(&'static self) -> &'static ForeignKey<I> {
-            std::mem::transmute(self)
+            unsafe { std::mem::transmute(self) }
         }
     }
 
